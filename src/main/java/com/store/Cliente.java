cc58d3889116548a0d6f7faa9196a50bca481da1
RadulfMenendo <r.menendez011010@gmail.com>
2024-05-13 18:58:25 -0600

    Client V1

--- src/main/java/com/store/Cliente.java
+++ src/main/java/com/store/Cliente.java
@@ -11,19 +11,24 @@ public class Cliente {
 
         try (BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in))) {
 
+            // Solicitar dirección del servidor
             System.out.println("Escriba la dirección del servidor: ");
             String host = br1.readLine();
             System.err.println("\n\nEscriba el puerto:");
             int pto = Integer.parseInt(br1.readLine());
 
-            try (
-                Socket cl = new Socket(host, pto);
-                BufferedReader br2 = new BufferedReader(new InputStreamReader(cl.getInputStream()))
-            ) {
+            // Establecer conexión con el servidor y recibimos mensajes
+            try (Socket cl = new Socket(host, pto);
+                    BufferedReader br2 = new BufferedReader(new InputStreamReader(cl.getInputStream()))) {
+
+                // Creamos variable para leer el mensaje del servidor
                 String mensaje;
+
+                // Ciclo para leer los mensajes del servidor
                 while ((mensaje = br2.readLine()) != null) {
                     System.out.println(mensaje);
                 }
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
