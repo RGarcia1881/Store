b2480e3cab426f4506df5073e5eb82a0456bbc08
RadulfMenendo <r.menendez011010@gmail.com>
2024-05-14 02:19:10 -0600

    Cart working

--- src/main/java/com/store/Cliente.java
+++ src/main/java/com/store/Cliente.java
@@ -2,7 +2,7 @@ package com.store;
 
 import java.io.*;
 import java.net.*;
-import java.util.List;
+import java.util.*;
 
 //172.100.64.82
 
@@ -33,30 +33,76 @@ public class Cliente {
                 for (Product producto : productos) {
                     System.out.println(producto);
                 }
+
+                // Iniciamos el carrito de compra
+                Cart carrito = new Cart();
+
+                // Declaramos una variable para almacenar el índice del producto seleccionado
+                int opcion;
+
+                do {
+
+                    // Mostrar menú
+                    System.out.println("\n--- Menú ---");
+                    System.out.println("1. Agregar producto al carrito");
+                    System.out.println("2. Ver contenido del carrito");
+                    System.out.println("3. Eliminar producto del carrito");
+                    System.out.println("4. Vaciar carrito");
+                    System.out.println("5. Salir");
+
+                    // Leer la opción seleccionada
+                    System.out.print("Seleccione una opción: ");
+                    opcion = Integer.parseInt(br1.readLine());
+
+                    // Realizar la acción correspondiente a la opción seleccionada
+
+                    switch (opcion) {
+                        case 1:
+                            // Agregar producto al carrito
+                            System.out.print("Seleccione el índice del producto a agregar: ");
+                            int index = Integer.parseInt(br1.readLine());
+                            System.out.print("Indique la cantidad a agregar: ");
+                            int s = Integer.parseInt(br1.readLine());
+                            Item item = new Item(productos.get(index).getNombre(), productos.get(index).getPrecio(),
+                                    s - 1, productos.get(index).getDescripcion());
+                            System.out.println("\n" + item + "\n");
+                            carrito.addItem(item);
+                            System.out.println("Producto agregado al carrito");
+                            break;
+                        case 2:
+                            // Ver contenido del carrito
+                            System.out.println("Contenido del carrito:");
+                            System.out.println(carrito);
+                            break;
+                        case 3:
+                            // Eliminar producto del carrito
+                            System.out.print("Seleccione el índice del producto a eliminar: ");
+                            int index2 = Integer.parseInt(br1.readLine());
+                            carrito.removeItem(index2 - 1);
+                            System.out.println("Producto eliminado del carrito");
+                            break;
+                        case 4:
+                            // Vaciar carrito
+                            carrito.clear();
+                            System.out.println("Carrito vaciado");
+                            break;
+                        case 5:
+                            // Salir
+                            System.out.println("Saliendo...");
+                            break;
+                        default:
+                            // Opción inválida
+                            System.out.println("Opción inválida");
+                            break;
+                    }
+
+                    System.out.println(opcion);
+                } while (opcion != 5);
+
             } catch (IOException | ClassNotFoundException e) {
                 // Manejar cualquier excepción que pueda ocurrir al recibir o leer la lista
                 e.printStackTrace();
             }
-
-            /*
-             * try (Socket cl = new Socket(host, pto);
-             * BufferedReader br2 = new BufferedReader(new
-             * InputStreamReader(cl.getInputStream()))) {
-             * 
-             * // Creamos variable para leer el mensaje del servidor
-             * String mensaje;
-             * 
-             * // Ciclo para leer los mensajes del servidor
-             * while ((mensaje = br2.readLine()) != null) {
-             * System.out.println(mensaje);
-             * }
-             * 
-             * } catch (IOException e) {
-             * e.printStackTrace();
-             * }
-             */
-
-            // Hacemos el carrito de compra
 
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/com/store/Main.java
+++ src/main/java/com/store/Main.java
@@ -15,13 +15,13 @@ public class Main {
             connection = ConnectionDB.getConnection();
             System.out.println("Conexion exitosa a la base de datos");
 
-            /*
+            /* 
              * #############################################################################
              * ###########
              * Puebas CRUD a la Bases de Datos *
              * #############################################################################
              * ###########
-             */
+             
 
             // Agregar un nuevo producto
             ApiCart.agregarProducto(connection, "Mango", 15.75, 150, "Mango manila fresco");
@@ -33,47 +33,46 @@ public class Main {
             ApiCart.agregarProducto(connection, "Doritos", 18.00, 200, "Doritos nacho sin abrir");
             System.out.println("Producto agregado con éxito");
 
-            /*
-             * // Editar un producto existente
-             * ApiCart.editarProducto(connection, 1, "Mango", 15.75, 150,
-             * "Mango manila fresco");
-             * System.out.println("Producto editado con éxito");
-             * ApiCart.editarProducto(connection, 2, "Platano", 13.50, 100,
-             * "Amarillo amarillo platano");
-             * System.out.println("Producto editado con éxito");
-             * ApiCart.editarProducto(connection, 3, "Aguacate", 10.00, 120,
-             * "Aguacate hass fresco");
-             * System.out.println("Producto editado con éxito");
-             * ApiCart.editarProducto(connection, 4, "Doritos", 18.00, 200,
-             * "Doritos nacho sin abrir");
-             * System.out.println("Producto editado con éxito");
-             */
+        
+             // Editar un producto existente
+              ApiCart.editarProducto(connection, 1, "Mango", 15.75, 150,
+              "Mango manila fresco");
+              System.out.println("Producto editado con éxito");
+              ApiCart.editarProducto(connection, 2, "Platano", 13.50, 100,
+              "Amarillo amarillo platano");
+              System.out.println("Producto editado con éxito");
+              ApiCart.editarProducto(connection, 3, "Aguacate", 10.00, 120,
+              "Aguacate hass fresco");
+              System.out.println("Producto editado con éxito");
+              ApiCart.editarProducto(connection, 4, "Doritos", 18.00, 200,
+              "Doritos nacho sin abrir");
+             System.out.println("Producto editado con éxito");
 
-        } catch (SQLException e) {
-        }
+        
+        //Eliminar un producto
+          ApiCart.eliminarProducto(connection, 3);
+          System.out.println("Producto eliminado con éxito");
+         
 
-        /*
-         * Eliminar un producto
-         * ApiCart.eliminarProducto(connection, 3);
-         * System.out.println("Producto eliminado con éxito");
-         */
+        
+         //Ver todos los productos
+         List<Product> productos = ApiCart.obtenerProductos(connection);
+         System.out.println("Lista de productos:");
+         for (Product producto : productos) {
+         System.out.println(producto);
+         }
+        
 
-        /*
-         * Ver todos los productos
-         * List<Product> productos = ApiCart.obtenerProductos(connection);
-         * System.out.println("Lista de productos:");
-         * for (Product producto : productos) {
-         * System.out.println(producto);
-         * }
-         */
+        
+         //Obtener un producto por ID
+         System.out.println("Producto " + 1);
+         int id = 2;
+         Product producto = ApiCart.obtenerProducto(connection, id);
+         System.out.println(producto);
+         
+         
+*/
 
-        /*
-         * Obtener un producto por ID
-         * System.out.println("Producto " + 1);
-         * int id = 2;
-         * Product producto = ApiCart.obtenerProducto(connection, id);
-         * System.out.println(producto);
-         */
 
         /*
          * #############################################################################
@@ -83,7 +82,7 @@ public class Main {
          * ###########
          */
 
-        /*
+        
          * Cart cart = new Cart();
          * // Menú de opciones
          * int opcion;
@@ -128,101 +127,99 @@ public class Main {
          * System.out.println("Conexion cerrada");
          * scanner.close();
          * }
-         * }
-         */
-        /*
-         * #############################################################################
-         * ###########
-         * Funciones para las Puebas CRUD al carrito de compras *
-         * #############################################################################
-         * ###########
-         */
+         * }*/
+    /*
+     * #############################################################################
+     * ###########
+     * Funciones para las Puebas CRUD al carrito de compras *
+     * #############################################################################
+     * ###########
+     */
 
-        /*
-         * // Método para agregar un producto al carrito
-         * private static void agregarProductoAlCarrito(Connection connection, Scanner
-         * scanner, Cart cart)
-         * throws SQLException {
-         * // Solicitar al usuario que ingrese el ID del producto que desea agregar al
-         * // carrito
-         * System.out.print("Ingrese el ID del producto que desea agregar al carrito: "
-         * );
-         * int productId = scanner.nextInt();
-         * 
-         * // Obtener el producto de la base de datos utilizando el ID ingresado por el
-         * // usuario
-         * Product product = ApiCart.obtenerProducto(connection, productId);
-         * 
-         * // Verificar si se encontró el producto en la base de datos
-         * if (product != null) {
-         * // Solicitar al usuario que ingrese la cantidad de existencias que desea
-         * agregar
-         * // al carrito
-         * System.out.
-         * print("Ingrese la cantidad de existencias que desea agregar al carrito para el producto '"
-         * + product.getNombre() + "': ");
-         * int cantidad = scanner.nextInt();
-         * 
-         * // Crear un nuevo ítem con la información del producto y la cantidad
-         * ingresada
-         * // por el usuario, y agregarlo al carrito
-         * cart.addItem(new Item(product.getNombre(), product.getPrecio(), cantidad,
-         * product.getDescripcion()));
-         * System.out.println("Producto agregado al carrito con éxito.");
-         * } else {
-         * // Informar al usuario si el producto no fue encontrado en la base de datos
-         * System.out.println("El producto con ID " + productId +
-         * " no fue encontrado en la base de datos.");
-         * }
-         * }
-         * 
-         * // Método para ver el contenido del carrito
-         * private static void verContenidoDelCarrito(Cart cart) {
-         * // Verificar si el carrito está vacío
-         * if (cart.getItems().isEmpty()) {
-         * // Informar al usuario si el carrito está vacío
-         * System.out.println("El carrito está vacío.");
-         * } else {
-         * // Mostrar el contenido del carrito
-         * System.out.println("Contenido del carrito:");
-         * System.out.println(cart);
-         * }
-         * }
-         * 
-         * // Método para eliminar un producto del carrito
-         * private static void eliminarProductoDelCarrito(Scanner scanner, Cart cart) {
-         * // Verificar si el carrito está vacío
-         * if (cart.getItems().isEmpty()) {
-         * // Informar al usuario si el carrito está vacío y no hay productos para
-         * eliminar
-         * System.out.println("El carrito está vacío. No hay productos para eliminar.");
-         * } else {
-         * // Solicitar al usuario que ingrese el índice del producto que desea eliminar
-         * // del carrito
-         * System.out.
-         * print("Ingrese el índice del producto que desea eliminar del carrito: ");
-         * int index = scanner.nextInt();
-         * 
-         * // Verificar si el índice ingresado por el usuario es válido
-         * if (index >= 0 && index < cart.getItems().size()) {
-         * // Eliminar el producto del carrito utilizando el índice proporcionado por el
-         * // usuario
-         * cart.removeItem(index);
-         * System.out.println("Producto eliminado del carrito con éxito.");
-         * } else {
-         * // Informar al usuario si el índice ingresado es inválido
-         * System.out.println("Índice inválido. Intente de nuevo.");
-         * }
-         * }
-         * }
-         * 
-         * // Método para vaciar el carrito
-         * private static void vaciarCarrito(Cart cart) {
-         * // Vaciar el carrito eliminando todos los productos
-         * cart.clear();
-         * System.out.println("El carrito ha sido vaciado.");
-         * }
-         * 
-         */
-    }
-}
+    /*
+     * // Método para agregar un producto al carrito
+     * private static void agregarProductoAlCarrito(Connection connection, Scanner
+     * scanner, Cart cart)
+     * throws SQLException {
+     * // Solicitar al usuario que ingrese el ID del producto que desea agregar al
+     * // carrito
+     * System.out.print("Ingrese el ID del producto que desea agregar al carrito: "
+     * );
+     * int productId = scanner.nextInt();
+     * 
+     * // Obtener el producto de la base de datos utilizando el ID ingresado por el
+     * // usuario
+     * Product product = ApiCart.obtenerProducto(connection, productId);
+     * 
+     * // Verificar si se encontró el producto en la base de datos
+     * if (product != null) {
+     * // Solicitar al usuario que ingrese la cantidad de existencias que desea
+     * agregar
+     * // al carrito
+     * System.out.
+     * print("Ingrese la cantidad de existencias que desea agregar al carrito para el producto '"
+     * + product.getNombre() + "': ");
+     * int cantidad = scanner.nextInt();
+     * 
+     * // Crear un nuevo ítem con la información del producto y la cantidad
+     * ingresada
+     * // por el usuario, y agregarlo al carrito
+     * cart.addItem(new Item(product.getNombre(), product.getPrecio(), cantidad,
+     * product.getDescripcion()));
+     * System.out.println("Producto agregado al carrito con éxito.");
+     * } else {
+     * // Informar al usuario si el producto no fue encontrado en la base de datos
+     * System.out.println("El producto con ID " + productId +
+     * " no fue encontrado en la base de datos.");
+     * }
+     * }
+     * 
+     * // Método para ver el contenido del carrito
+     * private static void verContenidoDelCarrito(Cart cart) {
+     * // Verificar si el carrito está vacío
+     * if (cart.getItems().isEmpty()) {
+     * // Informar al usuario si el carrito está vacío
+     * System.out.println("El carrito está vacío.");
+     * } else {
+     * // Mostrar el contenido del carrito
+     * System.out.println("Contenido del carrito:");
+     * System.out.println(cart);
+     * }
+     * }
+     * 
+     * // Método para eliminar un producto del carrito
+     * private static void eliminarProductoDelCarrito(Scanner scanner, Cart cart) {
+     * // Verificar si el carrito está vacío
+     * if (cart.getItems().isEmpty()) {
+     * // Informar al usuario si el carrito está vacío y no hay productos para
+     * eliminar
+     * System.out.println("El carrito está vacío. No hay productos para eliminar.");
+     * } else {
+     * // Solicitar al usuario que ingrese el índice del producto que desea eliminar
+     * // del carrito
+     * System.out.
+     * print("Ingrese el índice del producto que desea eliminar del carrito: ");
+     * int index = scanner.nextInt();
+     * 
+     * // Verificar si el índice ingresado por el usuario es válido
+     * if (index >= 0 && index < cart.getItems().size()) {
+     * // Eliminar el producto del carrito utilizando el índice proporcionado por el
+     * // usuario
+     * cart.removeItem(index);
+     * System.out.println("Producto eliminado del carrito con éxito.");
+     * } else {
+     * // Informar al usuario si el índice ingresado es inválido
+     * System.out.println("Índice inválido. Intente de nuevo.");
+     * }
+     * }
+     * }
+     * 
+     * // Método para vaciar el carrito
+     * private static void vaciarCarrito(Cart cart) {
+     * // Vaciar el carrito eliminando todos los productos
+     * cart.clear();
+     * System.out.println("El carrito ha sido vaciado.");
+     * }
+     * 
+     */
+}}
